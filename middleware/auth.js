8ed03f8cd6c3814d7db420e39cee25d117328681
soroardiislam <mdsoroardi602828@gmail.com>
2025-11-27 22:07:20 +0600

    Logout

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,30 @@
-const auth = async(req, res)=>{
-  try {
-    const token = req.cookies.accessToken || req?.header?.authorization.splite("")[1];
+import jwt from "jsonwebtoken"
 
+const auth = async(req, res, next)=>{
+  try {
+    const token = req.cookies.accessToken || req?.header?.authorization.splite(" ")[1];
+
+    if(!token){
+        res.json({
+          message:"Provide the Token",
+          error: true,
+        })
+    }
+
+    const decode =await jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
+
+    if(!decode){
+      res.jsone({
+        message: "User are not Authorization",
+        error: true
+      })
+    }
+
+    req.userId = decode.id
     console.log("token",token)
-    
+ 
+    next()
+
   } catch (error) {
     res.json({
       message: error.message || message,
@@ -11,3 +32,5 @@ const auth = async(req, res)=>{
     })
   }
 }
+
+export default auth ;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,13 @@
 import {Router} from "express"
 import { userLogin, userLogout, UserRegister, verifyEmailController } from "../controllers/user.controllers.js"
+import auth from "../middleware/auth.js";
 
 const userRouter = Router()
 
 userRouter.post("/register", UserRegister);
 userRouter.post("/verify", verifyEmailController);
 userRouter.post("/login", userLogin);
-userRouter.post("/logout", userLogout);
+userRouter.post("/logout", auth, userLogout);
 
 
 export default userRouter;
